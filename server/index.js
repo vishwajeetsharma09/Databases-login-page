329e1031d0da10d229f1b7c2f5e4e66a72d175c9
@vishwajeetsharma09 <2000031822@kluniversity.in>
2024-05-31 00:28:25 +0530

    intial

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const RegisterModel = require("./models/Register");
 const app = express();
 app.use(
   cors({
-    origin: [""],
+    origin: ["https://databases-login-page-ykft.vercel.app"],
     methods: ["POST", "GET"],
     credentials: true,
   })
